8ec406a1b676f2c391609e0370a1814f94f69dab
Niyazi Sırt <niyazi.sirt@labrisnetworks.com>
2016-10-03 22:23:47 +0300

    import settings to url

--- ppatlov/urls.py
+++ ppatlov/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from ppatlov import views
+from ppatlov import views, settings
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
