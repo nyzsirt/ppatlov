7043117a7ff6a6338426f60e7f3b8842ae26ac3a
Niyazi Sırt <niyazi.sirt@labrisnetworks.com>
2016-10-06 12:40:52 +0300

    static serv

--- ppatlov/settings.py
+++ ppatlov/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
@@ -152,5 +152,7 @@ STATICFILES_DIRS = (
     # "/app/static",
 )
 
-
 STATIC_URL = '/static/'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
