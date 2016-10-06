6d9c799ca34ca9149e82369e52f65dfa05a0b092
Niyazi Sırt <niyazi.sirt@labrisnetworks.com>
2016-10-06 11:42:20 +0300

    add comment staticfiles

--- ppatlov/settings.py
+++ ppatlov/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
